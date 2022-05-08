b35ab1ad5dfbc0964392ecf1c13a34fa568a90c7
Stephen Roller <roller@fb.com>
2022-05-08 01:43:35 +0000

    [scripts] Convert resharded MP checkpoints to unflattened.

---
+++ metaseq/scripts/convert_to_singleton.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+"""
+Script for backing out of the MP-resharded (reshard.pt) files and getting back
+a non-flattened state dict.
+
+Particularly useful for converting our models to other repositories.
+
+Usage:
+    $ ls 125m
+    dict.txt
+    gpt2-merges.txt
+    gpt2-vocab.json
+    reshard-model_part-0.pt
+    reshard-model_part-1.pt
+
+    $ python -m metaseq.scripts.convert_to_singleton 125m
+
+    $ ls 125m
+    dict.txt
+    gpt2-merges.txt
+    gpt2-vocab.json
+    reshard-model_part-0.pt
+    reshard-model_part-1.pt
+    restored.pt
+"""
+
+import torch
+import argparse
+import glob
+
+from metaseq import options, tasks, checkpoint_utils, utils
+from metaseq.dataclass.configs import MetaseqConfig
+from metaseq.dataclass.utils import convert_namespace_to_omegaconf
+from metaseq.distributed import utils as dist_utils
+from metaseq.distributed import fsdp_enable_wrap, fsdp_wrap
+
+
+def worker_main(cfg: MetaseqConfig):
+    """
+    Load up the model on all workers for Model Parallelism, then
+    unflatten, move to cpu, and save to "restored.pt".
+    """
+    task = tasks.setup_task(cfg.task)
+
+    def _build_model(cfg, task):
+        # hardcoded to cpu & fp16
+        model = task.build_model(cfg.model).half().cpu()
+        return fsdp_wrap(model)
+
+    with fsdp_enable_wrap(
+        cfg.distributed_training,
+        use_sharded_state=cfg.distributed_training.use_sharded_state,
+    ):
+        models, _model_args, _task = checkpoint_utils.load_model_ensemble_and_task(
+            utils.split_paths(cfg.common_eval.path),
+            arg_overrides=None,
+            task=task,
+            suffix=cfg.checkpoint.checkpoint_suffix,
+            strict=True,
+            num_shards=cfg.checkpoint.checkpoint_shard_count,
+            build_model_hook=_build_model,
+        )
+        model = models[0]
+
+    if dist_utils.get_global_rank() == 0:
+        with open(cfg.task.data + "/restored.pt", "wb") as f:
+            torch.save(model.module.state_dict(), f)
+
+
+def main():
+    # parser to be used like docstring shows
+    real_parser = argparse.ArgumentParser()
+    real_parser.add_argument("location")
+    args = real_parser.parse_args()
+    files = glob.glob(f"{args.location}/reshard*.pt")
+
+    MP = len(files)
+    BPE_MERGES = args.location + "/gpt2-merges.txt"
+    BPE_VOCAB = args.location + "/gpt2-vocab.txt"
+
+    # Skeleton out all the annoying command line args we can infer
+    ARGS = [
+        "--model-parallel-size", str(MP),
+        "--distributed-world-size", str(MP),
+        "--task", "language_modeling",
+        "--bpe-merges", BPE_MERGES,
+        "--bpe-vocab", BPE_VOCAB,
+        "--bpe", "hf_byte_bpe",
+        "--path", args.location + "/reshard.pt",
+        "--checkpoint-shard-count", "1",
+        "--use-sharded-state",
+        args.location
+    ]
+
+    # build up the config file
+    parser = options.get_generation_parser()
+    # dumb defaults overriding
+    parser.set_defaults(lr_scheduler=None, criterion=None)
+    args = options.parse_args_and_arch(parser, input_args=ARGS)
+    cfg = convert_namespace_to_omegaconf(args)
+    cfg.distributed_training.distributed_world_size = MP
+    dist_utils.call_main(cfg, worker_main)
+
+
+if __name__ == '__main__':
+    main()
