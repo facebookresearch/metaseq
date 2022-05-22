7e30d75ecf74d7ab98c2a8b28099903567d2a399
Susan Zhang <susanz@fb.com>
2022-05-22 12:49:03 -0400

    remove module rename for progress bar

--- metaseq/__init__.py
+++ metaseq/__init__.py
@@ -19,12 +19,10 @@ __all__ = ["pdb"]
 # backwards compatibility to support `from metaseq.X import Y`
 from metaseq.distributed import utils as distributed_utils
 from metaseq.logging import meters, metrics  # noqa
-from metaseq.logging.progress_bar.utils import get_progress_bar
 
 sys.modules["metaseq.distributed_utils"] = distributed_utils
 sys.modules["metaseq.meters"] = meters
 sys.modules["metaseq.metrics"] = metrics
-sys.modules["metaseq.progress_bar"] = get_progress_bar
 
 # initialize hydra
 from metaseq.dataclass.initialize import hydra_init  # noqa: E402
