b9e5727fe95f56c413b26e2f4455a3e8ddca76e5
Susan Zhang <susanz@fb.com>
2022-07-03 15:53:13 +0200

    remove logic for removing layer norm in older versions of checkpoints

--- metaseq/models/transformer.py
+++ metaseq/models/transformer.py
@@ -919,13 +919,6 @@ class TransformerDecoder(IncrementalDecoder):
                         ] = state_dict[k]
                         del state_dict[k]
 
-        version_key = "{}.version".format(name)
-        if utils.item(state_dict.get(version_key, torch.Tensor([1]))[0]) <= 2:
-            # earlier checkpoints did not normalize after the stack of layers
-            self.layer_norm = None
-            self.normalize = False
-            state_dict[version_key] = torch.Tensor([1])
-
         return state_dict
 
 
