d21c7049affc856f51b2cddc5a04785577c82102
Susan Zhang <susanz@fb.com>
2022-07-04 15:25:58 +0200

    unused import

--- metaseq/models/base_encoder.py
+++ metaseq/models/base_encoder.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Dict, List, NamedTuple, Optional
+from typing import Dict, NamedTuple, Optional
 
 import torch
 import torch.nn as nn
