b56ede2a1e5c5cff9328ddb74bd05d4be8fed839
Punit Singh Koura <punitkoura@fb.com>
2022-07-11 19:15:24 +0000

    Adding code to configure the 125m model with HF dependencies for the integration test

--- gpu_tests/test_hf_compatibility.py
+++ gpu_tests/test_hf_compatibility.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 import os
-from transformers import AutoTokenizer, GPT2Tokenizer
+from transformers import GPT2Tokenizer
 from metaseq import checkpoint_utils
 from transformers import OPTForCausalLM
+from packaging import version
 import torch
 import unittest
 
 
 # forward passes
-def single_batch_forward_logits(model, tokenizer, prompts):
+def forward(model, tokenizer, prompts):
     input_ids = tokenizer(prompts, return_tensors="pt").input_ids
     input_ids = torch.cat([torch.tensor([[0]]), input_ids], dim=-1)
     input_ids = input_ids
@@ -16,19 +17,22 @@ def single_batch_forward_logits(model, tokenizer, prompts):
         logits = model(input_ids)[0]
     return logits
 
-# forward hf
-def forward_hf(hf_model, tokenizer, prompts):
-    input_ids = tokenizer(prompts, return_tensors="pt").input_ids
-    input_ids = torch.cat([torch.tensor([[0]]), input_ids], dim=-1)
-    input_ids = input_ids
-    with torch.no_grad():
-        logits = hf_model(input_ids)[0]
-    return logits
+
+def get_next_token(logits, tokenizer):
+    pred_next_token = torch.argmax(logits[0, -1], -1)
+    next_token = tokenizer.convert_ids_to_tokens([pred_next_token])
+    next_token = next_token[0].replace("Ġ", "")
+    return next_token
+
 
 @unittest.skipIf(not torch.cuda.is_available(), "test requires a GPU")
+@unittest.skipIf(
+    version.parse(torch.__version__) < version.parse("1.9.1"),
+    "test requires a pytorch version of at least 1.9.1",
+)
 class TestTraining(unittest.TestCase):
     def test_metaseq_hf_compatibility(self):
-        model_path = "./125m"
+        model_path = os.path.join(os.path.dirname(__file__), "125m")
 
         vocab_file = os.path.join(model_path, "gpt2-vocab.json")
         merges_file = os.path.join(model_path, "gpt2-merges.txt")
@@ -41,7 +45,7 @@ class TestTraining(unittest.TestCase):
             arg_overrides={
                 "vocab_filename": vocab_file,
                 "merges_filename": merges_file,
-            }
+            },
         )
 
         model = checkpoint[0][0].eval()
@@ -57,18 +61,16 @@ class TestTraining(unittest.TestCase):
         ]
 
         for prompt in prompts:
-            logits_metaseq = single_batch_forward_logits(model, tokenizer, prompt)
-            pred_next_token = torch.argmax(logits_metaseq[0, -1], -1)
-            metaseq_next_token = tokenizer.convert_ids_to_tokens([pred_next_token])
-            metaseq_next_token = metaseq_next_token[0].replace("Ġ", "")
+            logits_metaseq = forward(model, tokenizer, prompt)
+            metaseq_next_token = get_next_token(logits_metaseq, tokenizer)
 
-            logits_hf = forward_hf(model, tokenizer, prompt)
-            pred_next_token = torch.argmax(logits_hf[0, -1], -1)
-            hf_next_token = tokenizer.convert_ids_to_tokens([pred_next_token])
-            hf_next_token = hf_next_token[0].replace("Ġ", "")
+            logits_hf = forward(hf_model, tokenizer, prompt)
+            hf_next_token = get_next_token(logits_hf, tokenizer)
 
             # Assert that HF and metaseq versions of the same model predict the same logits
-            self.assertTrue(torch.allclose(logits_metaseq.cpu(), logits_hf.cpu(), atol=1e-3))
+            self.assertTrue(
+                torch.allclose(logits_metaseq.cpu(), logits_hf.cpu(), atol=1e-3)
+            )
 
             # Assert that HF and metaseq versions of the same model predict the same word
             self.assertEqual(metaseq_next_token, hf_next_token)
