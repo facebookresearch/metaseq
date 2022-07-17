677e55421024fba76d3d2d0a477f2da074703acf
Susan Zhang <susanz@fb.com>
2022-07-17 19:11:04 +0200

    add **kwargs to forward in test

--- tests/utils.py
+++ tests/utils.py
@@ -424,7 +424,9 @@ class TestIncrementalDecoder(IncrementalDecoder):
         args.max_decoder_positions = getattr(args, "max_decoder_positions", 100)
         self.args = args
 
-    def forward(self, prev_output_tokens, encoder_out=None, incremental_state=None):
+    def forward(
+        self, prev_output_tokens, encoder_out=None, incremental_state=None, **kwargs
+    ):
         if incremental_state is not None:
             prev_output_tokens = prev_output_tokens[:, -1:]
         bbsz = prev_output_tokens.size(0)
