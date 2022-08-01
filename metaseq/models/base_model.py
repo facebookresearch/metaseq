38ac9c780404cc7ecf4681da1dd4c5ea9715cd99
Srini Iyer <sviyer@fb.com>
2022-08-01 23:24:45 +0000

    Fix base model to match base decoder

--- metaseq/models/base_model.py
+++ metaseq/models/base_model.py
@@ -55,28 +55,18 @@ class BaseModel(nn.Module):
         """Get targets from either the sample or the net's output."""
         return sample["target"]
 
-    def get_normalized_probs(
-        self,
-        net_output: Tuple[Tensor, Optional[Dict[str, List[Optional[Tensor]]]]],
-        log_probs: bool,
-        sample: Optional[Dict[str, Tensor]] = None,
-    ):
+    def get_normalized_probs(self, logits: Tensor, log_probs: bool):
         """Get normalized probabilities (or log probs) from a net's output."""
-        return self.get_normalized_probs_scriptable(net_output, log_probs, sample)
+        return self.get_normalized_probs_scriptable(logits, log_probs)
 
     # TorchScript doesn't support super() method so that the scriptable Subclass
     # can't access the base class model in Torchscript.
     # Current workaround is to add a helper function with different name and
     # call the helper function from scriptable Subclass.
-    def get_normalized_probs_scriptable(
-        self,
-        net_output: Tuple[Tensor, Optional[Dict[str, List[Optional[Tensor]]]]],
-        log_probs: bool,
-        sample: Optional[Dict[str, Tensor]] = None,
-    ):
-        """Scriptable helper function for get_normalized_probs in ~BaseModel"""
+    def get_normalized_probs_scriptable(self, logits: Tensor, log_probs: bool):
+        """Get normalized probabilities (or log probs) from a net's output."""
         if hasattr(self, "decoder"):
-            return self.decoder.get_normalized_probs(net_output, log_probs, sample)
+            return self.decoder.get_normalized_probs(logits, log_probs)
         elif torch.is_tensor(net_output):
             # syntactic sugar for simple models which don't have a decoder
             # (e.g., the classification tutorial)
