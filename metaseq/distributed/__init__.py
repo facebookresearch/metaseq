40a2c5dfa39ae482597b1c1c1eb111c83b60c365
Anna Vordou <annavordou@csd.auth.gr>
2022-09-30 01:31:53 +0300

    Delete Heartbeat Timeout

--- metaseq/distributed/__init__.py
+++ metaseq/distributed/__init__.py
@@ -3,7 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .distributed_timeout_wrapper import DistributedTimeoutWrapper
 from .fully_sharded_data_parallel import (
     fsdp_enable_wrap,
     fsdp_wrap,
@@ -13,7 +12,6 @@ from .module_proxy_wrapper import ModuleProxyWrapper
 
 
 __all__ = [
-    "DistributedTimeoutWrapper",
     "fsdp_enable_wrap",
     "fsdp_wrap",
     "FullyShardedDataParallel",
