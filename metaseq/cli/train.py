bd069a038c94a6d27c3231baa2c3abfac54b2316
Srinivasan Iyer <sviyer@fb.com>
2022-09-29 13:04:17 -0700

    wandb logging to the same folder as training logs (#362)

--- metaseq/cli/train.py
+++ metaseq/cli/train.py
@@ -255,6 +255,7 @@ def train(
         wandb_run_name=os.environ.get(
             "WANDB_NAME", os.path.basename(cfg.checkpoint.save_dir)
         ),
+        wandb_dir=cfg.checkpoint.save_dir,
     )
     progress.update_config(_flatten_config(cfg))
 
@@ -682,6 +683,7 @@ def validate(
                 wandb_run_name=os.environ.get(
                     "WANDB_NAME", os.path.basename(cfg.checkpoint.save_dir)
                 ),
+                wandb_dir=cfg.checkpoint.save_dir,
             )
 
             logger.info(
