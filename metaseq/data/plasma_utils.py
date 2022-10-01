0a24830bb7f70a3f47b2bbd512885276275e8f38
Susan Zhang <susanz@fb.com>
2022-10-01 16:09:06 -0500

    remove unused PlasmaView class

--- metaseq/data/plasma_utils.py
+++ metaseq/data/plasma_utils.py
@@ -4,11 +4,8 @@
 # LICENSE file in the root directory of this source tree.
 
 
-import hashlib
-import json
 import subprocess
 import tempfile
-from typing import Hashable
 
 try:
     import pyarrow.plasma as plasma
@@ -95,87 +92,6 @@ class PlasmaArray:
 
 
 DEFAULT_PLASMA_PATH = "/tmp/plasma"
-
-
-class PlasmaView:
-    """Interface to write and read from shared memory. Whereas PlasmaArray writes to plasma on serialization,
-    PlasmaView writes to shared memory on instantiation."""
-
-    def __init__(self, array, split_path: str, hash_data: Hashable, plasma_path=None):
-        """
-        Args:
-            array: numpy array to store. This can be read with ``PlasmaView().array``
-            split_path: the path whence the data was read, used for hashing
-            hash_data: other metadata about the array that can be used to create a unique key.
-                as of writing, the 3 callers in ``TokenBlockDataset`` use::
-
-                    hash_data = ((block_size, document_sep_len, str(break_mode), len(dataset)), 0|1|2)
-
-
-        """
-        assert PYARROW_AVAILABLE
-        assert split_path is not None
-        if plasma_path is None:
-            plasma_path = DEFAULT_PLASMA_PATH
-
-        self.path = plasma_path
-        self.split_path = split_path
-        self._client = None  # Initialize lazily for pickle. plasma clients should not be deep copied or serialized.
-        self._n = None
-
-        self.object_id = self.get_object_id(self.split_path, hash_data)
-        try:
-            self.client.put(array, object_id=self.object_id)
-        except plasma.PlasmaObjectExists:
-            pass
-
-    @property
-    def client(self):
-        if self._client is None:
-            self._client = plasma.connect(self.path, num_retries=200)
-        return self._client
-
-    @property
-    def array(self):
-        """Fetch a read only view of an np.array, stored in plasma."""
-        ret = self.client.get(self.object_id)
-        return ret
-
-    @staticmethod
-    def get_object_id(split_path: str, hash_data: Hashable):
-        """Returns plasma.ObjectID from hashing split_path and object_num."""
-        hash = hashlib.blake2b(bytes(split_path, "utf-8"), digest_size=20)
-        harg = json.dumps(hash_data).encode("utf-8")
-        hash.update(harg)
-        return plasma.ObjectID(hash.digest())
-
-    def __getstate__(self):
-        """Called on pickle save"""
-        self.disconnect()
-        state = self.__dict__.copy()
-        assert state["_client"] is None
-        assert "object_id" in state
-        return state
-
-    def __setstate__(self, state):
-        """Called on pickle load"""
-        self.__dict__.update(state)
-
-    def __del__(self):
-        self.disconnect()
-
-    def disconnect(self):
-        if self._client is not None:
-            self._client.disconnect()
-            self._client = None
-
-    def __len__(self):
-        """Save reads by caching len"""
-        if self._n is None:
-            self._n = len(self.array)
-        return self._n
-
-
 GB100 = (1024**3) * 100
 
 
