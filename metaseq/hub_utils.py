d0945a9a1f8aea2c1d46a8397f738f2ccbb565cb
Punit Singh Koura <punitkoura@fb.com>
2022-10-13 17:15:01 +0000

    Lint

--- metaseq/hub_utils.py
+++ metaseq/hub_utils.py
@@ -816,7 +816,7 @@ class GeneratorInterface:
         if distributions is not None:
             # If we broke early in the loop above, ensure that we
             # fill mask with False upto distributions.shape[0]
-            mask.extend([False]*(distributions.shape[0] - len(mask)))
+            mask.extend([False] * (distributions.shape[0] - len(mask)))
             distributions = distributions[mask]
 
         return list(new_tokens), list(new_scores), distributions
