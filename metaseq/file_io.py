8afd768aa01843f407757376d070b39973fdd311
Susan Zhang <susanz@fb.com>
2022-10-15 10:12:31 -0500

    cleanup imports

--- metaseq/file_io.py
+++ metaseq/file_io.py
@@ -5,11 +5,10 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from argparse import Namespace  # noqa: F401
-import json
 import logging
 import os
 import shutil
+from argparse import Namespace  # noqa: F401
 from typing import List, Optional
 
 import torch
