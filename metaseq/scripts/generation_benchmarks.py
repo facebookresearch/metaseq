24f93c262d3daebedc15694ed58f90a4f3cf5ba9
Susan Zhang <susanz@fb.com>
2022-11-10 21:40:01 +0100

    add headers

--- metaseq/scripts/generation_benchmarks.py
+++ metaseq/scripts/generation_benchmarks.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import os
 from transformers import GPT2Tokenizer
 from metaseq import checkpoint_utils, tasks, utils
