7993bcf21a57b394932df4a870fec23f5d200b41
Stephen Roller <roller@fb.com>
2022-11-11 12:54:03 -0500

    [distributed] Custom rendezvous (#510)
    * [distributed] Custom rendezvous
    
    * More logging.

--- metaseq/distributed/__init__.py
+++ metaseq/distributed/__init__.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import metaseq.distributed.rendezvous  # noqa: F401
 from .fully_sharded_data_parallel import (
     fsdp_enable_wrap,
     fsdp_wrap,
