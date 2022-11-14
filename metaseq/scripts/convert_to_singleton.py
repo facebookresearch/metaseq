d703cf1ae1e0faaff6c20629398dfbe02b98cf77
Susan Zhang <suchenzang@users.noreply.github.com>
2022-11-14 14:26:47 +0100

    add headers (#514)

--- metaseq/scripts/convert_to_singleton.py
+++ metaseq/scripts/convert_to_singleton.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 """
 Script for backing out of the MP-resharded (reshard.pt) files and getting back
 a non-flattened state dict.
