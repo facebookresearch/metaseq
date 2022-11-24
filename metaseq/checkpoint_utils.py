c00105cf2c5cc525ef4fa3c5a80f0f5445bbeab9
Susan Zhang <susanz@fb.com>
2022-11-24 14:06:39 -0600

    save all the lasts

--- metaseq/checkpoint_utils.py
+++ metaseq/checkpoint_utils.py
@@ -74,7 +74,9 @@ def save_checkpoint(
     checkpoint_conds[f"checkpoint{epoch}{suffix}.pt"] = save_for_epoch
     checkpoint_conds[f"checkpoint_{updates}{suffix}.pt"] = save_for_updates
     checkpoint_conds[f"checkpoint_last{suffix}.pt"] = (
-        training_finished and cfg.save_last_checkpoint
+        (training_finished and cfg.save_last_checkpoint)
+        or save_for_epoch
+        or save_for_updates
     )
 
     extra_state = {"train_iterator": epoch_itr.state_dict()}
