849734cbb07948aa9ec4d01a27819b440fc8790d
Ubuntu <ubuntu@ip-172-31-31-190.ec2.internal>
2022-12-27 17:04:31 +0000

    pin numpy version

--- setup.py
+++ setup.py
@@ -154,7 +154,7 @@ def do_setup(package_data):
             "more_itertools",
             "mypy",
             "ninja",
-            'numpy; python_version>="3.7"',
+            'numpy==1.24.0; python_version>="3.7"',
             "omegaconf<=2.1.1",
             "portalocker>=2.5",
             "pre-commit",
