a8e391139b2d01bd7bb4f461bd66171d2117dbb3
Zachary DeVito <zdevito@gmail.com>
2023-01-04 08:50:47 -0800

    check model parallel ranks have consistent data (#566)
    * check model parallel ranks have consistent data
    
    * torch.save needs to be on main thread
    
    * patch up broken iterators

--- metaseq/trainer.py
+++ metaseq/trainer.py
@@ -403,13 +403,12 @@ class Trainer(object):
                 def perform_save():
                     try:
                         logger.info(f"Beginning asynchronous torch.save to {filename}")
-                        torch.save(state_dict, filename)
                         if async_callback_fn is not None:
                             async_callback_fn(filename)
                         logger.info(f"Asynchronous torch.save to {filename} complete.")
                     except Exception as e:
                         logger.exception(f"Asynchronous save failed: {e}")
-
+                torch.save(state_dict, filename)
                 self.async_checkpoint.submit(perform_save)
             logger.info(f"Finished saving checkpoint to {filename}")
 
@@ -1057,6 +1056,30 @@ class Trainer(object):
 
         if self.cuda:
             sample = utils.move_to_cuda(sample)
+
+            if False: # turn on to double-check we do not have data loader issues
+                # When we finish an epoch some dataloaders run short on data one iteration before others.
+                # We want to check that the data loaders that are running short are returning correct data
+                # on all their previous iterations.
+
+                # If they are returning the correct data, then we can rule out a lot of reasons why they would
+                # run short.
+
+                ipt = sample['net_input']['src_tokens']
+                if not hasattr(self, 'input_errors'):
+                    self.input_errors = torch.tensor(0, dtype=torch.int, device=ipt.device)
+
+                min_ipt = ipt.clone()
+
+                torch.distributed.all_reduce(
+                    min_ipt, op=torch.distributed.ReduceOp.MIN, group=distributed_utils.get_model_parallel_group()
+                )
+
+                self.input_errors += (min_ipt != ipt).any()
+
+                if self.get_num_updates() % self.cfg.common.log_interval == 0:
+                    if int(self.input_errors) > 0:
+                        logger.error(f"Data {self.data_parallel_rank} Model {distributed_utils.get_model_parallel_rank()} has {self.input_errors} data mismatch errors!")
 
         def lower_precision(t):
             """Converts a tensor to the desired dtype based on our cfg."""
