6170df1654b7fa687702a7092981d618540518cf
bashnick <bashnick@gmail.com>
2023-01-11 17:04:57 +0000

    enable second test only

--- gpu_tests/test_model_parallel_mp1_mp2.py
+++ gpu_tests/test_model_parallel_mp1_mp2.py
@@ -18,7 +18,6 @@ class TestModelParallel(unittest.TestCase):
     The tests checks hat the number of trianing steps performed is correct
     and that the required loss is achieved on the last iteration
     """
-
     def test_model_parallel_mp1(self):
         self.assertEqual(1,1)
         # # run a 8M model with 1 model parallel (mp1)
@@ -55,39 +54,39 @@ class TestModelParallel(unittest.TestCase):
         # )  # one decimal point precision
 
     def test_model_parallel_mp2(self):
-        self.assertEqual(1,1)
-    #     # run a 8M model with 2 model parallels (mp2)
-    #     mp2_results = subprocess.Popen(
-    #         "python3 metaseq/launcher/opt_baselines.py \
-    #         --prefix train.8m --model-size 8m --checkpoints-dir ./test-checkpoint \
-    #         --tensorboard-logdir ./test-checkpoint --num-trials 1 --azure \
-    #         --num-gpus 4 --num-nodes 1 --seed 1 \
-    #         --local --disable-validation --max-epoch 5 --max-update 5 --benchmark".split(),
-    #         stdout=subprocess.PIPE,
-    #         stderr=subprocess.PIPE,
-    #         universal_newlines=True,
-    #     )
-    #     mp2_stdout, _ = mp2_results.communicate()
+        # self.assertEqual(1,1)
+        # run a 8M model with 2 model parallels (mp2)
+        mp2_results = subprocess.Popen(
+            "python3 metaseq/launcher/opt_baselines.py \
+            --prefix train.8m --model-size 8m --checkpoints-dir ./test-checkpoint \
+            --tensorboard-logdir ./test-checkpoint --num-trials 1 --azure \
+            --num-gpus 4 --num-nodes 1 --seed 1 \
+            --local --disable-validation --max-epoch 5 --max-update 5 --benchmark".split(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        )
+        mp2_stdout, _ = mp2_results.communicate()
 
-    #     # cleanup generated checkpoints files
-    #     cleanup_checkpoints = subprocess.Popen(
-    #         "rm -r ./test-checkpoint".split(),
-    #         stdout=subprocess.PIPE,
-    #         stderr=subprocess.PIPE,
-    #         universal_newlines=True,
-    #     )
-    #     _, _ = cleanup_checkpoints.communicate()
+        # cleanup generated checkpoints files
+        cleanup_checkpoints = subprocess.Popen(
+            "rm -r ./test-checkpoint".split(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        )
+        _, _ = cleanup_checkpoints.communicate()
 
-    #     # mp2: check that the training was successfull
-    #     training_mp2_log_events = re.findall(r'{"epoch.*"}', mp2_stdout)
-    #     last_epoch_mp2_loss = float(json.loads(training_mp2_log_events[-1])["loss"])
+        # mp2: check that the training was successfull
+        training_mp2_log_events = re.findall(r'{"epoch.*"}', mp2_stdout)
+        last_epoch_mp2_loss = float(json.loads(training_mp2_log_events[-1])["loss"])
 
-    #     # check that number of steps performed is correct
-    #     self.assertEqual(len(training_mp2_log_events), 10)
-    #     # check that the achieved loss is correct
-    #     self.assertAlmostEqual(
-    #         last_epoch_mp2_loss, 10.48, 1
-    #     )  # one decimal point precision
+        # check that number of steps performed is correct
+        self.assertEqual(len(training_mp2_log_events), 10)
+        # check that the achieved loss is correct
+        self.assertAlmostEqual(
+            last_epoch_mp2_loss, 10.48, 1
+        )  # one decimal point precision
 
 
 if __name__ == "__main__":
