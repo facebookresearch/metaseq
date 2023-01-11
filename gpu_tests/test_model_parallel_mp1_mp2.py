9c87734d266eb4034e3299f4b5d64f5b7461edc4
bashnick <bashnick@gmail.com>
2023-01-11 14:13:37 +0000

    test smaller test size

--- gpu_tests/test_model_parallel_mp1_mp2.py
+++ gpu_tests/test_model_parallel_mp1_mp2.py
@@ -53,39 +53,39 @@ class TestModelParallel(unittest.TestCase):
             last_epoch_mp1_loss, 10.318, 1
         )  # one decimal point precision
 
-    def test_model_parallel_mp2(self):
-        # run a 8M model with 2 model parallels (mp2)
-        mp2_results = subprocess.Popen(
-            "python3 metaseq/launcher/opt_baselines.py \
-            --prefix train.8m --model-size 8m --checkpoints-dir ./test-checkpoint \
-            --tensorboard-logdir ./test-checkpoint --num-trials 1 --azure \
-            --num-gpus 4 --num-nodes 1 --seed 1 \
-            --local --disable-validation --max-epoch 5 --max-update 5 --benchmark".split(),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            universal_newlines=True,
-        )
-        mp2_stdout, _ = mp2_results.communicate()
+    # def test_model_parallel_mp2(self):
+    #     # run a 8M model with 2 model parallels (mp2)
+    #     mp2_results = subprocess.Popen(
+    #         "python3 metaseq/launcher/opt_baselines.py \
+    #         --prefix train.8m --model-size 8m --checkpoints-dir ./test-checkpoint \
+    #         --tensorboard-logdir ./test-checkpoint --num-trials 1 --azure \
+    #         --num-gpus 4 --num-nodes 1 --seed 1 \
+    #         --local --disable-validation --max-epoch 5 --max-update 5 --benchmark".split(),
+    #         stdout=subprocess.PIPE,
+    #         stderr=subprocess.PIPE,
+    #         universal_newlines=True,
+    #     )
+    #     mp2_stdout, _ = mp2_results.communicate()
 
-        # cleanup generated checkpoints files
-        cleanup_checkpoints = subprocess.Popen(
-            "rm -r ./test-checkpoint".split(),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            universal_newlines=True,
-        )
-        _, _ = cleanup_checkpoints.communicate()
+    #     # cleanup generated checkpoints files
+    #     cleanup_checkpoints = subprocess.Popen(
+    #         "rm -r ./test-checkpoint".split(),
+    #         stdout=subprocess.PIPE,
+    #         stderr=subprocess.PIPE,
+    #         universal_newlines=True,
+    #     )
+    #     _, _ = cleanup_checkpoints.communicate()
 
-        # mp2: check that the training was successfull
-        training_mp2_log_events = re.findall(r'{"epoch.*"}', mp2_stdout)
-        last_epoch_mp2_loss = float(json.loads(training_mp2_log_events[-1])["loss"])
+    #     # mp2: check that the training was successfull
+    #     training_mp2_log_events = re.findall(r'{"epoch.*"}', mp2_stdout)
+    #     last_epoch_mp2_loss = float(json.loads(training_mp2_log_events[-1])["loss"])
 
-        # check that number of steps performed is correct
-        self.assertEqual(len(training_mp2_log_events), 10)
-        # check that the achieved loss is correct
-        self.assertAlmostEqual(
-            last_epoch_mp2_loss, 10.48, 1
-        )  # one decimal point precision
+    #     # check that number of steps performed is correct
+    #     self.assertEqual(len(training_mp2_log_events), 10)
+    #     # check that the achieved loss is correct
+    #     self.assertAlmostEqual(
+    #         last_epoch_mp2_loss, 10.48, 1
+    #     )  # one decimal point precision
 
 
 if __name__ == "__main__":
