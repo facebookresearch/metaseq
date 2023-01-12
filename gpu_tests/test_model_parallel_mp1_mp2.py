6a281602755bc3a6e3ba6b7e5173127e0a49af8f
bashnick <bashnick@gmail.com>
2023-01-12 12:50:01 +0000

    revert to the original test to share

--- gpu_tests/test_model_parallel_mp1_mp2.py
+++ gpu_tests/test_model_parallel_mp1_mp2.py
@@ -1,13 +1,9 @@
 import re
 import subprocess
 import json
-import multiprocessing
 import unittest
-from unittest.mock import patch
-from metaseq.launcher.opt_baselines import cli_main as sweep_cli_main
 import torch
 from metaseq.dataclass.configs import DistributedTrainingConfig
-import logging
 
 
 @unittest.skipIf(not torch.cuda.is_available(), "test requires 4 GPUs, none found")
@@ -16,17 +12,6 @@ import logging
     "test requires 4 GPUs",
 )
 
-def run_training(events, max_update):
-    argv_injection = (
-        "python3 metaseq/launcher/opt_baselines.py   "
-        "--prefix train.8m    --model-size 8m    --checkpoints-dir ./test-checkpoint    "
-        "--tensorboard-logdir ./test-checkpoint    --num-trials 1    --azure   "
-        "--num-gpus 4 --num-nodes 1   --seed 1   "
-        "--local --disable-validation    --max-epoch 5    --max-update 5 --benchmark    "
-    )
-    with patch("sys.argv", argv_injection.split()[1:]):
-        sweep_cli_main()
-
 class TestModelParallel(unittest.TestCase):
     """
     These tests will verify that the model can be trained with
@@ -34,23 +19,22 @@ class TestModelParallel(unittest.TestCase):
     The tests checks hat the number of trianing steps performed is correct
     and that the required loss is achieved on the last iteration
     """
-    def test_model_parallel_mp0(self):
-        max_update_first_run = 20
-        multiprocessing.set_start_method("spawn", force=True)
-        with torch.multiprocessing.Manager() as manager:
-            events = manager.list()
-            p = multiprocessing.Process(
-                target=run_training,
-                args=(
-                    events,
-                    max_update_first_run,
-                ),
-            )
-            p.start()
-            p.join()
-            events_first_run = list(events)
 
-            # cleanup generated checkpoints files
+    def test_model_parallel_mp2(self):
+        # run a 8M model with 2 model parallel (mp2)
+        mp1_results = subprocess.Popen(
+            "python3 metaseq/launcher/opt_baselines.py \
+            --prefix train.8m --model-size 8m --checkpoints-dir ./test-checkpoint \
+            --tensorboard-logdir ./test-checkpoint --num-trials 1 --azure \
+            --num-gpus 4 --num-nodes 1 --seed 1 \
+            --local --disable-validation --max-epoch 5 --max-update 5 --benchmark".split(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        )
+        mp1_stdout, _ = mp1_results.communicate()
+
+        # cleanup generated checkpoints files
         cleanup_checkpoints = subprocess.Popen(
             "rm -r ./test-checkpoint".split(),
             stdout=subprocess.PIPE,
@@ -59,106 +43,16 @@ class TestModelParallel(unittest.TestCase):
         )
         _, _ = cleanup_checkpoints.communicate()
 
-        self.assertEqual(1,1)
+        # mp1: check that the training was successfull
+        training_mp1_log_events = re.findall(r'{"epoch.*"}', mp1_stdout)
+        last_epoch_mp1_loss = float(json.loads(training_mp1_log_events[-1])["loss"])
 
-
-    def test_model_parallel_mp1(self):
-        self.assertEqual(1,1)
-        # # run a 8M model with 1 model parallel (mp1)
-        # mp1_results = subprocess.Popen(
-        #     "python3 metaseq/launcher/opt_baselines.py \
-        #     --prefix train.8m --model-size 8m_mp1 --checkpoints-dir ./test-checkpoint \
-        #     --tensorboard-logdir ./test-checkpoint --num-trials 1 --azure \
-        #     --num-gpus 4 --num-nodes 1 --seed 1 \
-        #     --local --disable-validation --max-epoch 5 --max-update 5 --benchmark".split(),
-        #     stdout=subprocess.PIPE,
-        #     stderr=subprocess.PIPE,
-        #     universal_newlines=True,
-        # )
-        # mp1_stdout, _ = mp1_results.communicate()
-
-        # # cleanup generated checkpoints files
-        # cleanup_checkpoints = subprocess.Popen(
-        #     "rm -r ./test-checkpoint".split(),
-        #     stdout=subprocess.PIPE,
-        #     stderr=subprocess.PIPE,
-        #     universal_newlines=True,
-        # )
-        # _, _ = cleanup_checkpoints.communicate()
-
-        # # mp1: check that the training was successfull
-        # training_mp1_log_events = re.findall(r'{"epoch.*"}', mp1_stdout)
-        # last_epoch_mp1_loss = float(json.loads(training_mp1_log_events[-1])["loss"])
-
-        # # check that number of steps performed is correct
-        # self.assertEqual(len(training_mp1_log_events), 10)
-        # # check that the achieved loss is correct
-        # self.assertAlmostEqual(
-        #     last_epoch_mp1_loss, 10.318, 1
-        # )  # one decimal point precision
-
-    def test_model_parallel_mp2(self):
-        self.assertEqual(1,1)
-        # run a 8M model with 2 model parallels (mp2)
-
-        # logger = logging.getLogger(__name__)
-        # logger.info(f"LOGGING BEGINS <><>")
-
-        # command = ["python3", "metaseq/launcher/opt_baselines.py",
-        #     "--prefix", "train.8m",
-        #     "--model-size", "8m",
-        #     "--checkpoints-dir", "./test-checkpoint",
-        #     "--tensorboard-logdir", "./test-checkpoint",
-        #     "--num-trials", "1",
-        #     "--azure",
-        #     "--num-gpus", "4",
-        #     "--num-nodes", "1",
-        #     "--seed", "1",
-        #     "--local",
-        #     "--disable-validation",
-        #     "--max-epoch", "5",
-        #     "--max-update", "5",
-        #     "--benchmark"]
-
-        # mp2_results = subprocess.Popen(command,
-        #     stdout=subprocess.PIPE,
-        #     stderr=subprocess.PIPE,
-        #     universal_newlines=True,
-        # )
-
-        # mp2_stdout, mp2_stderr = mp2_results.communicate()
-        # print("mp2_stdout: ", mp2_stdout)
-        # print("mp2_stderr: ", mp2_stderr)
-
-        # logger.info(f"mp2_stdout: {mp2_stdout}")
-        # logger.info(f"mp2_stderr: {mp2_stderr}")
-
-        # logger.debug(f"mp2_stdout: {mp2_stdout}")
-        # logger.debug(f"mp2_stderr: {mp2_stderr}")
-
-        # logger.error(f"mp2_stdout: {mp2_stdout}")
-        # logger.error(f"mp2_stderr: {mp2_stderr}")
-
-        # # cleanup generated checkpoints files
-        # cleanup_checkpoints = subprocess.Popen(
-        #     "rm -r ./test-checkpoint".split(),
-        #     stdout=subprocess.PIPE,
-        #     stderr=subprocess.PIPE,
-        #     universal_newlines=True,
-        # )
-        # _, _ = cleanup_checkpoints.communicate()
-
-        # # mp2: check that the training was successfull
-        # training_mp2_log_events = re.findall(r'{"epoch.*"}', mp2_stdout)
-        # last_epoch_mp2_loss = float(json.loads(training_mp2_log_events[-1])["loss"])
-
-        # # check that number of steps performed is correct
-        # self.assertEqual(len(training_mp2_log_events), 10)
-        # # check that the achieved loss is correct
-        # self.assertAlmostEqual(
-        #     last_epoch_mp2_loss, 10.48, 1
-        # )  # one decimal point precision
-
+        # check that number of steps performed is correct
+        self.assertEqual(len(training_mp1_log_events), 10)
+        # check that the achieved loss is correct
+        self.assertAlmostEqual(
+            last_epoch_mp1_loss, 10.48, 1
+        )  # one decimal point precision
 
 if __name__ == "__main__":
     unittest.main()
