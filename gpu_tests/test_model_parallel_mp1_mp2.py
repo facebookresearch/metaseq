dda6fb4cea90c8778af3afca58f7edfc9749f9a0
bashnick <bashnick@gmail.com>
2023-01-13 11:27:37 +0000

    try remove num_workers parameter

--- gpu_tests/test_model_parallel_mp1_mp2.py
+++ gpu_tests/test_model_parallel_mp1_mp2.py
@@ -58,9 +58,6 @@ class TestModelParallel(unittest.TestCase):
             if event["type"] == "log" and event["message"].startswith('{"epoch"')
         ]
 
-        # to do
-        self.assertEqual(1, 1)
-
         # cleanup
         cleanup_checkpoints = subprocess.Popen(
             "rm -r ./test-checkpoint".split(),
@@ -71,6 +68,11 @@ class TestModelParallel(unittest.TestCase):
         _, _ = cleanup_checkpoints.communicate()
 
 
+        # to do
+        print("training_log_events: ", events_first_run)
+        self.assertEqual(1, 2)
+
+
 def run_training(events, max_update):
     argv_injection = (
         "python3 metaseq/launcher/opt_baselines.py   "
@@ -78,8 +80,8 @@ def run_training(events, max_update):
         "--tensorboard-logdir ./test-checkpoint    --num-trials 1    --azure   "
         "--num-gpus 4 --num-nodes 1   --seed 1   "
         "--local --disable-validation    --max-epoch 5    --max-update 5 --benchmark    "
-        # "--full-azure-upload-path https://myaccount.blob.core.windows.net/test   "
     )
+    # both pathces needed to run the job of the circleci GPUs
     with patch("sys.argv", argv_injection.split()[1:]), patch(
         "metaseq.launcher.slurm.local_run",
         partial(local_run_mock, max_update=max_update, events=events),
@@ -91,123 +93,13 @@ def run_training(events, max_update):
 
 
 def local_run_mock(args, env, train_cmd, dry_run, max_update, events):
-    train_cmd[train_cmd.index("--max-update") + 1] = str(max_update)
-    train_cmd[train_cmd.index("--log-interval") + 1] = "1"
-    train_cmd[train_cmd.index("--save-interval-updates") + 1] = "18"
-    train_cmd[train_cmd.index("--num-workers") + 1] = "1"
+    # train_cmd[train_cmd.index("--max-update") + 1] = str(max_update)
+    # train_cmd[train_cmd.index("--log-interval") + 1] = "1"
+    # train_cmd[train_cmd.index("--save-interval-updates") + 1] = "18"
+    # train_cmd[train_cmd.index("--num-workers") + 1] = "1"
     with patch.dict("os.environ", env, clear=True):
         with patch("sys.argv", train_cmd[1:]):
-            # with patch(
-            #     "metaseq.distributed.utils.distributed_main",
-            #     partial(distributed_main_mock, events=events),
-            # ):
             train_cli_main()
-
-
-# def distributed_main_mock(i, main, cfg, kwargs, events):
-#     # need to patch this seperately here, otherwise spawns won't be patched
-#     with patch("logging.Logger._log", partialmethod(log_to_events, events=events)):
-#         with patch(
-#             "metaseq.cli.train._run_azcopy",
-#             partial(subprocess_run_mock, events=events),
-#         ):
-#             # need to mock this because the async part break the mock
-#             with patch(
-#                 "metaseq.cli.train.Trainer.save_checkpoint",
-#                 partialmethod(save_checkpoint_mock),
-#             ):
-#                 with patch("metaseq.cli.train.os.remove"):
-#                     mock_metaseq_internal = MagicMock()
-#                     mock_metaseq_internal.azure_utils.download_recent_ckpt = partial(
-#                         download_checkpoint_mock, events=events
-#                     )
-#                     sys.modules["metaseq_internal"] = mock_metaseq_internal
-#                     distributed_main(i, main, cfg, kwargs)
-
-
-# def download_checkpoint_mock(blob_url, checkpoint_path, suffix, events):
-#     # mocks the download of the checkpoint from azure
-#     _, checkpoint_dir, checkpoint_model_dir, checkpoint_file = checkpoint_path.split(
-#         "/"
-#     )
-#     events.append(
-#         {
-#             "type": "download",
-#             "blob_url": blob_url,
-#             "checkpoint_dir": checkpoint_dir,
-#             "checkpoint_model_dir": checkpoint_model_dir,
-#             "checkpoint_file": checkpoint_file,
-#             "suffix": suffix,
-#         }
-#     )
-
-#     return True
-
-
-# def subprocess_run_mock(cmd, stdout, stderr, events):
-#     # replaces subprocess.run azcopy command that uploads to azure
-#     _, checkpoint_dir, checkpoint_model_dir, checkpoint_file = cmd[4].split("/")
-#     events.append(
-#         {
-#             "type": "upload",
-#             "command": cmd[:4] + cmd[5:],
-#             "checkpoint_dir": checkpoint_dir,
-#             "checkpoint_model_dir": checkpoint_model_dir,
-#             "checkpoint_file": checkpoint_file,
-#             "file_saved_locally": os.path.exists(cmd[4]),
-#         }
-#     )
-
-#     res = Mock()
-#     res.returncode = 0
-#     return res
-
-
-# def save_checkpoint_mock(
-#     self, filename, extra_state, training_finished=False, async_callback_fn=None
-# ):
-#     logger = logging.getLogger("metaseq.trainer")
-#     """Save all training state in a checkpoint file."""
-#     # call state_dict on all ranks in case it needs internal communication
-#     state_dicts = self.state_dict(filename, training_finished)
-#     for filename, state_dict in state_dicts.items():
-#         logger.info(f"Saving checkpoint to {filename}")
-#         state_dict = metaseq_utils.move_to_cpu(
-#             state_dict,
-#             # keep params in FP16 when training with --memory-efficient-fp16
-#             cast_to_fp32=not self.cfg.common.memory_efficient_fp16,
-#         )
-#         state_dict["extra_state"].update(extra_state)
-#         if self.should_save_checkpoint_on_current_rank:
-#             # remove async part which break the patch
-#             # if not hasattr(self, "async_checkpoint"):
-#             #     self.async_checkpoint = ThreadPoolExecutor(max_workers=1)
-
-#             def perform_save():
-#                 try:
-#                     logger.info(f"Beginning asynchronous torch.save to {filename}")
-#                     torch.save(state_dict, filename)
-#                     if async_callback_fn is not None:
-#                         async_callback_fn(filename)
-#                     logger.info(f"Asynchronous torch.save to {filename} complete.")
-#                 except Exception as e:
-#                     logger.exception(f"Asynchronous save failed: {e}")
-
-#             # remove async part which break the patch
-#             # self.async_checkpoint.submit(perform_save)
-#             perform_save()
-#         logger.info(f"Finished saving checkpoint to {filename}")
-
-
-# def log_to_events(self, info, message, args, events, **kwargs):
-#     print(self, message)
-#     if isinstance(message, str):
-#         events.append(
-#             {
-#                 "type": "log",
-#                 "message": message,
-#             }
-#         )
 
 
 if __name__ == "__main__":
