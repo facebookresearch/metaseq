c4959c26a073e7a9edb5322935bcd8cedd712200
bashnick <bashnick@gmail.com>
2023-01-13 19:02:33 +0000

    split into two models

--- gpu_tests/test_model_parallel_mp2.py
+++ gpu_tests/test_model_parallel_mp2.py
@@ -5,18 +5,15 @@
 
 import subprocess
 import json
-import logging
 import multiprocessing
 from functools import partial, partialmethod
 import unittest
-from unittest.mock import patch, Mock, MagicMock
 import torch
+from unittest.mock import patch
 from metaseq.dataclass.configs import DistributedTrainingConfig
 from metaseq.launcher.opt_baselines import cli_main as sweep_cli_main
 from metaseq.cli.train import cli_main as train_cli_main
-from metaseq.distributed.utils import distributed_main
 from metaseq.launcher.opt_job_constants import Size, M
-import metaseq.utils as metaseq_utils
 
 
 @unittest.skipIf(not torch.cuda.is_available(), "test requires 4 GPUs, none found")
@@ -26,38 +23,11 @@ import metaseq.utils as metaseq_utils
 )
 class TestModelParallel(unittest.TestCase):
     """
-    These tests will verify that the model can be trained with
-    model_parallel = 1 and model_parallel = 2
-    The tests checks hat the number of trianing steps performed is correct
+    The test will verify that the model can be trained with
+    model_parallel = 2
+    The test checks hat the number of trianing steps performed is correct
     and that the required loss is achieved on the last iteration
     """
-
-    def test_model_parallel_mp1(self):
-        # change the model run arguments to run with 1 model parallel: --model-size 8m_mp1
-        argv_injection = (
-            "python3 metaseq/launcher/opt_baselines.py   "
-            "--prefix train.8m    --model-size 8m_mp1    --checkpoints-dir ./test-checkpoint    "
-            "--tensorboard-logdir ./test-checkpoint    --num-trials 1    --azure   "
-            "--num-gpus 4 --num-nodes 1   --seed 1   "
-            "--local --disable-validation    --max-epoch 5    --max-update 5 --benchmark    "
-        )
-        max_update_first_run = 20
-
-        training_log_events = self._test_model_parallel(
-            argv_injection, max_update_first_run
-        )
-
-        # check that training ran correctly
-        # check that the number of updates was correct
-        self.assertIsNotNone(training_log_events)
-        self.assertIsNotNone(training_log_events[-1])
-        self.assertIsNotNone(training_log_events[-1]["num_updates"])
-        self.assertEqual(
-            int(training_log_events[-1]["num_updates"]), max_update_first_run
-        )
-        # check the achieved loss is correct
-        loss_val = float(training_log_events[-1]["loss"])
-        self.assertAlmostEqual(loss_val, 14.736, 1)  # 1 digit precision
 
     def test_model_parallel_mp2(self):
         # change the model run arguments to run with 2 model parallel: --model-size 8m
