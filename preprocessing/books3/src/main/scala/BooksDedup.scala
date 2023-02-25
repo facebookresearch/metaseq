475a1b29531fc3a49d1b968d46e0d72d7487687b
suchenzang <suchenzang@gmail.com>
2023-02-25 06:00:17 -0500

    headers

--- preprocessing/books3/src/main/scala/BooksDedup.scala
+++ preprocessing/books3/src/main/scala/BooksDedup.scala
@@ -1,3 +1,8 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
 
--- preprocessing/books3/src/main/scala/BooksHeaderCleaner.scala
+++ preprocessing/books3/src/main/scala/BooksHeaderCleaner.scala
@@ -1,3 +1,8 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 import org.apache.log4j.{Level, LogManager}
 
 import org.apache.spark.sql._
