266deb9f314bba1ea80f8cef06199ea5dcacb7a0
Eric Smith <EricMichaelSmith@users.noreply.github.com>
2023-05-24 09:58:20 -0400

    Update configs.py

--- metaseq/dataclass/configs.py
+++ metaseq/dataclass/configs.py
@@ -639,7 +639,7 @@ class GenerationConfig(MetaseqDataclass):
     sampling_topk: int = field(
         default=-1,
         metadata={
-            "help": "sample from the top k next words"
+            "help": "sample from among the top k next tokens"
         },
     )
     temperature: float = field(
